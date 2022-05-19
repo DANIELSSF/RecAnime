ca897754e9a58f2b79bf14e4f79274d1f5c09a46
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-18 22:29:38 -0500

    was added a data from api with a url animeSearch

---
+++ src/helpers/getAnimeSearch.js
@@ -0,0 +1,23 @@
+
+export const getAnimeSearch = async (name) => {
+
+    const url = `https://api.jikan.moe/v4/anime?q=${name}`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+
+    const nameAnime = data.map(anime => {
+        return {
+            id: anime.mal_id,
+            img: anime.images.large_image_url,
+            title: anime.title,
+            source: anime.source,
+            episodes: anime.episodes,
+            status: anime.status,
+            score: anime.score,
+            synopsis: anime.synopsis,
+            year: anime.year
+        }
+    })
+    return nameAnime;
+}
