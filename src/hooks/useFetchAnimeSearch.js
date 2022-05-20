4f32a5b509ffe9794e65c947df04d171eb9e5508
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-20 15:16:22 -0500

    Loading and propTypes was added

--- src/hooks/useFetchAnimeSearch.js
+++ src/hooks/useFetchAnimeSearch.js
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react"
 import { getAnimeSearch } from "../helpers/getAnimeSearch";
+import PropTypes from 'prop-types';
 
 export const useFetchAnimeSearch = (name) => {
     const [animeSearch, setAnimeSearch] = useState({
-        data: []
+        data: [],
+        loading: false
     });
 
     useEffect(() => {
         getAnimeSearch(name)
             .then(anime => {
                 setAnimeSearch({
-                    data: anime
+                    data: anime,
+                    loading: true
                 });
             });
 
     }, [name]);
 
     return animeSearch;
+}
+
+useFetchAnimeSearch.propTypes = {
+    name: PropTypes.func.isRequired
 }
