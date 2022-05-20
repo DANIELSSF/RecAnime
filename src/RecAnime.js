76fe1bca6883bec922f12e85a1801853a6425449
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-19 21:31:08 -0500

    reference was made to the component AnimeSearch and AnimeSeacrhGrid

--- src/RecAnime.js
+++ src/RecAnime.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AnimeSearch } from './components/AnimeSearch';
+import { AnimeSearchGrid } from './components/AnimeSearchGrid';
 import { Recomendations } from './components/Recomendations';
 
 export const RecAnime = () => {
+    const [animeSearch, setAnimeSearch] = useState(['']);
+
 
     return (
         <>
             <h1> RecAnime </h1>
+            <AnimeSearch setAnimeSearch={setAnimeSearch} />
             <hr />
+            <ol>
+                {
+                        animeSearch.map(name =>
+                            <AnimeSearchGrid
+                                key={name}
+                                name={name} />)
+                }
+            </ol>
             <h2>Animes Recomendados!</h2>
             <ol>
                 {
