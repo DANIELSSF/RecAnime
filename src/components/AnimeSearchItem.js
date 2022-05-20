94d4d079da6b2dca4318ae6a92e8ce1be1fac6fd
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-20 15:14:45 -0500

    animation was added

--- src/components/AnimeSearchItem.js
+++ src/components/AnimeSearchItem.js
@@ -3,12 +3,14 @@ import React from 'react'
 export const AnimeSearchItem = ({ id, url, title, synopsis, source, episodes, status, score, year }) => {
 
     return (
-        <div>
-            <p>{title}</p>
-            <p>Year:{year} | Episodes:{episodes} | Status:{status}</p>
-            <img src={url} alt={title}></img> <p>Synopsis:{synopsis}</p>
-            <p>Score:{score} | Source:{source}</p>
-        </div>
+        <>
+            <div className='cardSearch animate__animated animate__zoomIn '>
+                <p>{title}</p>
+                <p>Year:{year} | Episodes:{episodes} | Status:{status}</p>
+                <img src={url} alt={title}></img> <p>Synopsis:{synopsis}</p>
+                <p>Score:{score} | Source:{source}</p>
+            </div>
+        </>
     )
 
 }
