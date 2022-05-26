3703cef6870a0ea2995ebee672f7f6aaadb0b349
Daniel Silva <daniel.santiago730@gmail.com>
2022-05-25 19:46:11 -0500

    this component was modified for use the hook useFetchRecomendations.js

--- src/components/Recomendations.js
+++ src/components/Recomendations.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import { useFetchRecomendations } from '../hooks/useFetchRecomendations'
+import { useRecomendations } from '../hooks/useRecomendations';
 import { RecomendationsItem } from './RecomendationsItem';
 
 export const Recomendations = () => {
-    const { data: images, loading } = useFetchRecomendations();
+    const {data,loading} = useFetchRecomendations();
     return (
         <>
-            {loading && <p className='animate__animated animate__zoomIn'>Loading...</p>}
+            {!loading && <p className='animate__animated animate__zoomIn'>Loading...</p>}
             <div className='card-grid'>
                 {
-                    images.map(img => (
+                    data.map(img => (
                         <RecomendationsItem
-                            key={img.idkey}
+                            key={img.id}
                             {...img} />
                     ))
                 }
